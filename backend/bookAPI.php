794520b12d7f0ebde57bb863618b59087f2bb3a1
Henry <henry.karppinen1997@gmail.com>
2025-05-28 15:26:28 +0300

    Changed allowed origin to backend

--- backend/bookAPI.php
+++ backend/bookAPI.php
@@ -1,5 +1,5 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://bookapp-64f12.web.app/");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
